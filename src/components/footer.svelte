6d88b879f615406cfe6bed34dc8ca4c10636f680
mxmlnhrc <maxihenri03@gmail.com>
2022-04-05 21:41:30 +0200

    added footer and changed style classes (Glass look)

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,6 +1,7 @@
 
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
-    <div>
-      <p></p>
+<footer class="footer footer-center p-4 bg-white text-base-content bg-opacity-25 backdrop-blur-2xl backdrop-saturate-100 border border-white border-opacity-10">
+    <div class="flex flex-row justify-around">
+      <p>© MH</p>
+      <p>2022</p>
     </div>
-  </footer>
+</footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+	import Navigation from '../components/nav.svelte';
+	import Footer from '../components/footer.svelte';
 </script>
 
+
 <slot />
+<Footer />
