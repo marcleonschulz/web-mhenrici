5e95cebf390a062df24a055a0742e3f259254d57
mxmlnhrc <maxihenri03@gmail.com>
2022-04-05 20:45:37 +0200

    added costume colors: background, accent, banner, grey and white

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		colors: {
+			background: '#000000',
+			accent: '#19B3D4',
+			banner: '#1B1B1B',
+			grey: '#E5E5E5',
+			white: '#F2F2F2',
+		},
 		extend: {}
 	},
 
