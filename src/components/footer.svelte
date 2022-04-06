171662915261d77a39320430fb25e96a5c418fca
mxmlnhrc <maxihenri03@gmail.com>
2022-04-06 16:00:57 +0200

    added footer and styling

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
 	export let text = '';
+	export let svgpath = '';
+	export let company = '';
+	export let year = '';
 </script>
 
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+<footer class="footer footer-center p-5 bg-white bg-opacity-40 backdrop-blur-md backdrop-brightness-75 text-white">
 	<div>
-		<p>{text}</p>
-	</div>
-</footer>
+		<img src="{svgpath}" alt="Logo" class="h-10 w-10" />
+	  <p class="">
+		{company} <br>{text}
+	  </p> 
+	  <p>Copyright © {year} - All rights reserved</p>
+	</div> 
+  </footer>
 
 <style>
 </style>
